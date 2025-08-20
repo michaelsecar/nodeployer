d4d3970b8cdc8e0e79cf248d06f348c219333298
Michael Secarlos AC <michaelsecar@gmail.com>
2025-08-20 17:09:02 -0500

    Update deploy endpoint

--- src/controllers/deploy.js
+++ src/controllers/deploy.js
@@ -1,9 +1,20 @@
 
+import { join } from 'path';
+import os from 'os'
+import fs from 'fs';
+
 export const deploy = async (req, res) =>  {
     try {
-        const data= req.body;
-        console.log(data);
-        if(!data) throw new Error("No content")
+        const data = req.body;
+        const file = req.file;
+        if(!data) throw new Error("No se han enviado datos dentro de la solicitud");
+        if(!file) throw new Error("No se ha enviado un archivo");
+
+        const filePath = join(os.homedir(), data.path)
+        if(!fs.existsSync(filePath) ){
+            fs.mkdirSync(filePath);
+        }
+        fs.copyFileSync(file.path, join(filePath, data.name));
 
         res.setHeader('Content-Type', 'application/json');
         res.send("ok");
