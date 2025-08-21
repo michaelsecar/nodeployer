b589c51924ab57c3bf67d090781899430240e07b
Michael Secarlos AC <michaelsecar@gmail.com>
2025-08-20 21:29:44 -0500

    Add parsing and validation to param service name at serviceStatus

--- src/controllers/status/status.js
+++ src/controllers/status/status.js
@@ -1,5 +1,6 @@
 
 import { exec } from 'child_process';
+import { parse } from 'path';
 
 export const status = (req, res) => {
     const response = JSON.stringify({
@@ -11,15 +12,18 @@ export const status = (req, res) => {
 
 export const serviceStatus = (req, res) => {
     try {
-        const body = req.body;
-        const serviceName = body.name;
-        exec(`systemctl status ${serviceName}`, (error, stdout, stderr) => {
+        if(!req.body.name) throw new Error('No se ha enviado el nombre del servicio');
+        const parsedName = req.body.name.split(' ')[0];
+        const isValidService = parsedName.endsWith('.service');
+        if(!isValidService) throw new Error('El nombre del servicio no es válido');
+
+        exec(`systemctl status ${parsedName}`, (error, stdout, stderr) => {
             //if (error) throw error;
             if(stderr) throw new Error(stderr);
             const isActive = stdout.includes('inactive') ? false: true;
             const response = JSON.stringify({
                 status: '200',
-                message: `El servicio ${serviceName} se encuentra ${isActive? "activo" : "inactivo"}`,
+                message: `El servicio ${parsedName} se encuentra ${isActive? "activo" : "inactivo"}`,
                 isActive: isActive
             })
             res.setHeader('Content-Type', 'application/json');
