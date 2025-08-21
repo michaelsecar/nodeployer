33e9665f3ea2a4836301e1b4cb341e999de72ae9
Michael Secarlos AC <michaelsecar@gmail.com>
2025-08-20 21:19:19 -0500

    Refactorize project files

--- src/controllers/status.js
+++
@@ -1,9 +0,0 @@
-
-export const status = (req, res) => {
-    const response = JSON.stringify({
-        status: '200',
-        message: 'OK!'
-    })
-    res.setHeader('Content-Type', 'application/json');
-    res.send(response);
-}
